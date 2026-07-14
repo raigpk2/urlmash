0d7c0ac2a1fc3f51fed0f9f53d3350cb2a9cf66e
raigpk2 <raigpk2@gmail.com>
2026-07-14 17:11:12 +0500

    Update browser.js

--- js/browser.js
+++ js/browser.js
@@ -1,1 +1,28 @@
-
+/**
+ * Browser Detection Logic
+ * Adds specific classes to the <html> element based on the user agent string.
+ */
+(function () {
+  const ua = navigator.userAgent.toLowerCase();
+  const html = document.documentElement;
+  if (/(iphone|ipod|ipad)/.test(ua)) {
+    html.classList.add('ios', 'mobile');
+  } else if (/android/.test(ua)) {
+    html.classList.add('android', 'mobile');
+  } else {
+    html.classList.add('desktop');
+  }
+  if (/chrome/.test(ua) && !/edg|brave/.test(ua)) {
+    html.classList.add('chrome');
+  } else if (/safari/.test(ua) && !/chrome/.test(ua)) {
+    html.classList.add('safari');
+  } else if (/edg/.test(ua)) {
+    html.classList.add('edge');
+  } else if (/firefox/.test(ua)) {
+    html.classList.add('firefox');
+  } else if (/brave/.test(ua)) {
+    html.classList.add('brave');
+  } else if (/opr|opera/.test(ua)) {
+    html.classList.add('opera');
+  }
+})();
