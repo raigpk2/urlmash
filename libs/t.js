b8d4d38f9ba84ded98328330540ba6c0aaf8167b
raigpk2 <raigpk2@gmail.com>
2026-07-14 17:10:40 +0500

    Update t.js

--- libs/t.js
+++ libs/t.js
@@ -1,1 +1,11 @@
-
+/**
+ * Whos.amung.us Widget Loader
+ * Dynamically loads the official third-party widget script to ensure 100% functionality 
+ * and avoid maintaining minified third-party code locally, adhering to best practices.
+ */
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://waust.at/t.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
